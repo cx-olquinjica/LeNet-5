2ac05b888fd3cadb0bebb0da2a4b5a5467ef137a
cx-olquinjica <aosiwaduo@outlook.com>
2023-05-10 11:28:46 +0800

    add:vgg_block

--- model/model.py
+++ model/model.py
@@ -112,3 +112,13 @@ class AlexNetMNIST(BaseModel):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
+# defining a vgg block
+
+def vgg_block(num_convs ,out_channels):
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.Conv2d(out_channels, kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+    layers.append(nn.MaxPool2d(kernel_size=3, stride=2))
+    return nn.Sequential(*layers)
+
