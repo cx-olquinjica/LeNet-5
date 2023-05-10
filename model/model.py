3e876f3633ae790d0e4de2c76713b2f10c20fe79
cx-olquinjica <aosiwaduo@outlook.com>
2023-05-10 10:31:45 +0800

    add:AlexNetMNIST

--- model/model.py
+++ model/model.py
@@ -71,3 +71,44 @@ class AlexNet(BaseModel):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
+
+class AlexNetMNIST(BaseModel):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.features = nn.Sequential(
+                nn.Conv2d(in_channels=1, out_channels=96, kernel_size=5, stride=1, padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(96, 256, kernel_size=5, padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(256, 384, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 384, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 256, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+        self.classifier = nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(256 * 6 * 6, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.5),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096, num_classes),
+        )
+
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0),-1 )
+        x = self.classifier(x)
+        return F.log_softmax(x, dim=1)
+
