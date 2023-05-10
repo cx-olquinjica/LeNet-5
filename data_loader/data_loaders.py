382c1481404518c554822203170dab93ebd60133
cx-olquinjica <aosiwaduo@outlook.com>
2023-05-10 10:06:14 +0800

    add:CIFAR10

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -12,5 +12,5 @@ class MnistDataLoader(BaseDataLoader):
             transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
