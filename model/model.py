b5b02461668722f3612e3e8743b687a03e5ef9d5
cx-olquinjica <aosiwaduo@outlook.com>
2023-05-10 14:01:15 +0800

    fix-typo:kernel_size

--- model/model.py
+++ model/model.py
@@ -119,6 +119,6 @@ def vgg_block(num_convs ,out_channels):
     for _ in range(num_convs):
         layers.append(nn.Conv2d(out_channels, kernel_size=3, padding=1))
         layers.append(nn.ReLU())
-    layers.append(nn.MaxPool2d(kernel_size=3, stride=2))
+    layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
     return nn.Sequential(*layers)
 
