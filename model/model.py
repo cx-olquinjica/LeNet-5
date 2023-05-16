a33c41d9ebd6029e4cd974d34888efb175f345bb
cx-olquinjica <aosiwaduo@outlook.com>
2023-05-16 10:50:32 +0800

    add:GoogLeNet

--- model/model.py
+++ model/model.py
@@ -195,3 +195,29 @@ class NiN(BaseModel):
         x = x.view(x.size(0), -1)
         return F.log_softmax(x, dim=1)
 
+# Inception block
+
+class Inception(nn.Module):
+    # C1--C4 are the number of output channels for each branch
+    def __init__(self, c1, c2, c3, c4, **kwargs):
+        super(Inception, self).__init__(**kwargs)
+        # Branch 1
+        self.b1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        # Branch 2
+        self.b2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.b2_2 = nn.Conv2d(in_channels, c2[1], kernel_size, padding=1)
+        # Branch 3
+        self.b3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.b3_2 = nn.Conv2d(in_channels, c3[1], kernel_size=5, padding=2)
+        # Branch 4
+        self.b4_1 = MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.b4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+        def forward(self, x): 
+            b1 = F.relu(self.b1_1(x))
+            b2 = F.relu(self.b2_2(F.relu(self.b2_1(x))))
+            b3 = F.relu(self.b3_2(F.relu(self.b3_1(x))))
+            b4 = F.relu(self.b4_2(self.b4_1(x)))
+            return torch.cat((b1, b2, b3, b4), dim=1)
+
+
